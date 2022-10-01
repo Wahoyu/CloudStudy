3854368c19c172c2f9de3b9a69542d6c4f72e275
王泓宇 <645702818@qq.com>
2022-10-01 21:52:58 +0800

    实现使用OpenFeign实现跨服务调用，负载均衡

---
+++ borrow-service/src/main/java/com/test/service/client/BookClient.java
@@ -0,0 +1,17 @@
+package com.test.service.client;
+
+import com.test.entity.Book;
+import com.test.entity.User;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@FeignClient("bookservice")
+public interface BookClient {
+    //路径保证和其他微服务提供的一致即可
+    @RequestMapping("/book/{bid}")
+    Book getBookById(@PathVariable("bid") int bid);  //参数和返回值也保持一致
+}
+
+
+
--- borrow-service/src/main/java/com/test/service/impl/BorrowServiceImpl.java
+++ borrow-service/src/main/java/com/test/service/impl/BorrowServiceImpl.java
@@ -6,6 +6,8 @@ import com.test.entity.User;
 import com.test.entity.UserBorrowDetail;
 import com.test.mapper.BorrowMapper;
 import com.test.service.BorrowService;
+import com.test.service.client.BookClient;
+import com.test.service.client.UserClient;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -13,6 +15,7 @@ import javax.annotation.Resource;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/*
 @Service
 public class BorrowServiceImpl implements BorrowService{
 
@@ -36,6 +39,36 @@ public class BorrowServiceImpl implements BorrowService{
                 .stream()
                 .map(b -> template.getForObject("http://bookservice/book/"+b.getBid(), Book.class))
                 .collect(Collectors.toList());
+        return new UserBorrowDetail(user, bookList);
+    }
+}*/
+@Service
+public class BorrowServiceImpl implements BorrowService {
+
+    @Resource
+    BorrowMapper mapper;
+
+    @Resource
+    UserClient userClient;
+
+    @Resource
+    BookClient bookClient;
+
+    @Override
+    public UserBorrowDetail getUserBorrowDetailByUid(int uid) {
+
+        //通过uid查询借阅信息
+        List<Borrow> borrow = mapper.getBorrowsByUid(uid);
+
+        //调用UserClient的方法查询user
+        User user = userClient.getUserById(uid);
+
+        //调用BookClient的方法查询数据列表
+        List<Book> bookList = borrow
+                .stream()
+                .map(b -> bookClient.getBookById(b.getBid()))
+                .collect(Collectors.toList());
+        //返回借阅信息
         return new UserBorrowDetail(user, bookList);
     }
 }
